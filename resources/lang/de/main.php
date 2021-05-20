85c9f2bf282dece24d58becfbd477f7ee7868a49
Alexander <alexander@makklays.com.ua>
2021-05-21 00:04:34 +0200

    add languages

--- resources/lang/de/main.php
+++ resources/lang/de/main.php
@@ -10,5 +10,42 @@ return [
     'Information' => 'Information',
     'blog' => 'Blog',
     'Company' => 'Company',
-    '' => '',
+
+    'home_title' => 'Latienda Title',
+    'home_description' => 'Latienda Description',
+    'home_keywords' => 'Latienda Keywords',
+
+    'about_title' => 'Latienda Title',
+    'about_description' => 'Latienda Description',
+    'about_keywords' => 'Latienda Keywords',
+
+    'delivery_title' => 'Latienda Title',
+    'delivery_description' => 'Latienda Description',
+    'delivery_keywords' => 'Latienda Keywords',
+
+    'contacts_title' => 'Latienda Title',
+    'contacts_description' => 'Latienda Description',
+    'contacts_keywords' => 'Latienda Keywords',
+
+    'products_title' => 'Latienda Title',
+    'products_description' => 'Latienda Description',
+    'products_keywords' => 'Latienda Keywords',
+    'one_product_title' => 'Latienda Title',
+    'one_product_description' => 'Latienda Description',
+    'one_product_keywords' => 'Latienda Keywords',
+
+    'categories_title' => 'Latienda Title',
+    'categories_description' => 'Latienda Description',
+    'categories_keywords' => 'Latienda Keywords',
+    'one_category_title' => 'Latienda Title',
+    'one_category_description' => 'Latienda Description',
+    'one_category_keywords' => 'Latienda Keywords',
+
+    'blogs_title' => 'Latienda Title',
+    'blogs_description' => 'Latienda Description',
+    'blogs_keywords' => 'Latienda Keywords',
+    'one_blog_title' => 'Latienda Title',
+    'one_blog_description' => 'Latienda Description',
+    'one_blog_keywords' => 'Latienda Keywords',
+    //
 ];
