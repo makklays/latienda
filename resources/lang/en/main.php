7301d3ec423bf01b60a50b228c4ebe227e342b9e
Alexander <alexander@makklays.com.ua>
2021-05-20 22:21:02 +0200

    add languages

---
+++ resources/lang/en/main.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'about' => 'About',
+    'home' => 'Home',
+    'delivery' => 'Delivery',
+    'categories' => 'Categories',
+    'contacts' => 'Contacts',
+    'Team' => 'General',
+    'Information' => 'Information',
+    'blog' => 'Blog',
+    'Company' => 'Company',
+    '' => '',
+];
---
+++ resources/lang/es/main.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'about' => 'Sobre',
+    'home' => 'Primera',
+    'delivery' => 'Entrega',
+    'categories' => 'Categorias',
+    'contacts' => 'Contactos',
+    'Team' => 'Generalmente',
+    'Information' => 'Información',
+    'blog' => 'Blog',
+    'Company' => 'Company',
+    '' => '',
+];
