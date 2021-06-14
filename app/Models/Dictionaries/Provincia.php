4947392c89e4cad62e86655a3cd130f437646e1a
Alexander <alexander@makklays.com.ua>
2021-06-14 20:19:06 +0200

    add issue 'Provincia'

---
+++ app/Models/Dictionaries/Provincia.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models\Dictionaries;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Provincia extends Model
+{
+    use HasFactory;
+
+    protected $table = 'd_provincia';
+
+    protected $fillable = [
+        'name_ru',
+        'name_en',
+        'name_es',
+    ];
+}
