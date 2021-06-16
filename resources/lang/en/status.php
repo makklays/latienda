dfcca26a560271e692b136a4efc9349064e78e97
Alexander <alexander@makklays.com.ua>
2021-06-16 13:39:57 +0200

    add files 'status' for different language

---
+++ resources/lang/en/status.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'noorder' => 'Not formalized',
+    'ordered' => 'Formalized',
+    'nopaid' => 'Not paid',
+    'paid' => 'Paid',
+    'packaged' => 'Packed',
+    'done' => 'Done',
+
+    'Status' => 'Status',
+    'Number' => 'Number',
+    'Date' => 'Date',
+    'Total' => 'Total',
+    'Action' => 'Action',
+    'Product' => 'Product',
+];
---
+++ resources/lang/es/status.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'noorder' => 'No estructurado',
+    'ordered' => 'Estructurado',
+    'nopaid' => 'No pagados',
+    'paid' => 'Pagados',
+    'packaged' => 'Envuelto',
+    'done' => 'Alcanzado plenamente',
+
+    'Status' => 'Estado',
+    'Number' => 'Numero',
+    'Date' => 'Fecha',
+    'Total' => 'Total',
+    'Action' => 'Acción',
+    'Product' => 'Producto',
+];
