5b2a4145c6c0731034bff8bb06c768250f55e1a5
Alexander <alexander@makklays.com.ua>
2021-09-19 21:08:37 +0200

    edit for MySQL (length)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // Add the following line
+        Schema::defaultStringLength(191);
     }
 
     /**
