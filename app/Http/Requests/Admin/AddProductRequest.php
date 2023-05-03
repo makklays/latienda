f5d3630ad0cdcf72b737ff0ce73cfb0272da70d2
Alexander Kuziv <alexander@makklays.com.ua>
2023-05-03 21:08:29 +0200

    Update AddProductRequest.php

--- app/Http/Requests/Admin/AddProductRequest.php
+++ app/Http/Requests/Admin/AddProductRequest.php
@@ -31,4 +31,29 @@ class AddProductRequest extends FormRequest
             'price' => 'required',
         ];
     }
+    
+    /**
+     * Message by language 
+     */
+    public function messages() : array
+    {
+        $lang = app()->getLocale();
+        
+        if ($lang == 'ru') {
+            return [
+                'sku.required' => 'Поле SKU обязательно для заполнения',
+                'title.required' => 'Поле Название обязательно для заполнения',
+            ];
+        } elseif ($lang == 'es') {
+            return [
+                'sku.required' => 'El campo SKU es obligatorio',
+                'title.required' => 'El campo Título es obligatorio',
+            ];
+        } else {
+            return [
+                'sku.required' => 'SKU is required',
+                'title.required' => 'Title is required',
+            ];
+        }
+    }
 }
